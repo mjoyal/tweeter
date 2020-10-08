306a25267f730f6dfb2093d39d0af8c83e3a8390
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-10-08 03:44:50 +0000

    retrieved the tweet data via AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,19 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
 $(document).ready(function () {
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  const $tweetContainer = $('#tweets-container'); 
 
-  const createTweetElement = (data) => {
+ const fetchTweets = () => {
+  $.ajax({
+    url: '/tweets', 
+    method: 'GET', 
+    dataType: 'json', 
+    success: (posts) => {renderTweet(posts)}, 
+    error: (error) => {console.error(error)}
+  });
+ }; 
+ fetchTweets(); 
+  const createTweetElement = function (data) {
     const $tweet = $(`<article>
       <header>
         <div class="user-tweet-info">
@@ -54,16 +38,27 @@ $(document).ready(function () {
     return $tweet; 
   };
 
-  const renderTweet = (tweetsArray) => {
-    for(const tweet of tweetsArray) {
+  const renderTweet = function (tweets) {
+    const $tweetContainer = $('#tweets-container'); 
+    $tweetContainer.empty(); 
+    for(const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $($tweetContainer).append($tweet); 
+      $($tweetContainer).prepend($tweet); 
 
     }
-  }; 
+  };  
 
-  renderTweet(tweetData); 
-  
+
+  $('#tweet-form').submit(function (event) {
+    event.preventDefault(); 
+    const serializedData = $(this).serialize(); 
+      $.post('/tweets', serializedData)
+        .then((response) => {
+          fetchTweets(); 
+      })
+  })
+
+
 });
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,19 @@
+
+
+
+
 $(document).ready(function() {
-  $('#tweet-text').on("input", function (event) {
-    const tweetVal = $(this).val()
-    const counter = $(this).siblings('div').children('.counter'); 
-    counter.text(140 - tweetVal.length);
-    if(parseInt(counter.text()) < 0) {
-      counter.css('color', 'red'); 
-    } else {
-      counter.css('color', '#545149'); 
-    }
-  }); 
+const countChars = function (event) {
+  const $tweetVal = $(this).val()
+  const $counter = $(this).siblings('div').children('.counter'); 
+  $counter.text(140 - $tweetVal.length);
+  if(parseInt($counter.text()) < 0) {
+    $counter.css('color', 'red'); 
+  } else {
+    $counter.css('color', '#545149'); 
+  }
+}; 
+  $('#tweet-text').on("input", countChars); 
+
 });
+
